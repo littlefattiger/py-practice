7049b3ccd3ddb803e36cf80ef80b3bb071f3139e
lilltfattiger <6698381@qq.com>
2022-05-06 21:48:33 -0700

    finish the basic for flask

--- framework/flask/zhiliao/app.py
+++ framework/flask/zhiliao/app.py
@@ -1,10 +1,13 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import config
 
 # from https://www.zlkt.net/book/detail/10/273
 from apps import user
 from apps import book
 from apps import course
+from flask_sqlalchemy import SQLAlchemy
+
+from form import LoginForm
 
 app = Flask(__name__)
 app.config.from_object(config)
@@ -28,7 +31,7 @@ def hello_world():  # put application's code here
 
 @app.route('/about')
 def about():
-    context = {"username":"apple12"}
+    context = {"username": "apple12"}
     return render_template('about.html', **context)
 
 
@@ -44,6 +47,38 @@ def book_detail(book_id):
 def book_list():
     return jsonify(books)
 
+
+# 数据库的配置变量
+HOSTNAME = 'hadoop102'
+PORT = '3306'
+DATABASE = 'db1'
+USERNAME = 'root'
+PASSWORD = '111111'
+DB_URI = 'mysql+pymysql://{}:{}@{}:{}/{}?charset=utf8'.format(USERNAME, PASSWORD, HOSTNAME, PORT, DATABASE)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URI
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+@app.route('/t1')
+def hello_t1():
+    engine = db.get_engine()
+    with engine.connect() as conn:
+        result = conn.execute("select 1")
+        print(result.fetchone())
+    return "test1"
+
+@app.route('/login', methods =['GET','POST'])
+def login():
+    if request.method=="GET":
+        return render_template("login.html")
+    else:
+        form = LoginForm(request.form)
+        if form.validate():
+            return "successful"
+        else:
+            return "email or password is wrong"
 
 if __name__ == '__main__':
     app.run()
